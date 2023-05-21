c9be8d873a8c707454f21fc2eebdd4ccec12ba38
hdulal38 <hari.dulal@outcodesoftware.com>
2023-05-21 14:52:18 +0545

    [UPDATE] add calendar

--- src/modules/dashboard/dashboard-page/dashboard-page.js
+++ src/modules/dashboard/dashboard-page/dashboard-page.js
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import { UserBillingsComponent } from "../../billings/user-billings";
 import { BooksBillingsComponent } from "../../billings/books-billing";
+import Calendar from "react-calendar";
+import './dashboardpage.scss';
 
 function DashboardPageComponent() {
     const isadmin = useSelector(state => state.authState.user?.isAdmin)
     return <div>
+        <div className="calendar"><Calendar /></div>
         {isadmin ? <UserBillingsComponent /> : <BooksBillingsComponent />}
     </div>
 }
